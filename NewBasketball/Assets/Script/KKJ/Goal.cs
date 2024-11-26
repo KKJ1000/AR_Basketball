674178c5aa9a014b6875058f024ca7279a830b98
KKJ1000 <kwangjoon1733@naver.com>
2024-11-26 11:22:37 +0900

    [Feat] 골 효과음 추가

--- NewBasketball/Assets/Script/KKJ/Goal.cs
+++ NewBasketball/Assets/Script/KKJ/Goal.cs
@@ -5,13 +5,18 @@ using UnityEngine.UI;
 
 public class Goal : MonoBehaviour
 {
+    AudioSource audioSource; //오디오 소스 컴포넌트
+    [SerializeField] private AudioClip goalAudioClip; //골 오디오 클립
     private Text scoreText;
     private int score; //점수
+
 
     void Start()
     {
         scoreText = FindObjectOfType<Text>();
         scoreText.text = $"Score : {score}";
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.clip = goalAudioClip;
     }
 
     void OnTriggerEnter(Collider other)
@@ -19,6 +24,7 @@ public class Goal : MonoBehaviour
         other.CompareTag("BALL");
         score++;
         scoreText.text = $"Score : {score}";
+        audioSource.Play();
     }
 
 }
